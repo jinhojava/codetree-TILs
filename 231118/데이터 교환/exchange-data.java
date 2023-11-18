501c804cfbfaec6bdfaae8cb1506fb2b6440dde0
jinhojava <139311543+jinhojava@users.noreply.github.com>
2023-11-18 18:14:12 +0900

    Title: 데이터 교환, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231118/데이터 교환/exchange-data.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        b=a;
+        c=b;
+        a=c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+}}
