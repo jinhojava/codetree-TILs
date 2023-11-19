b6752f684fdb9751d39f2c0fedd759eb2ba1eba7
jinhojava <139311543+jinhojava@users.noreply.github.com>
2023-11-19 23:53:42 +0900

    Title: 세 수의 중간값, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231119/세 수의 중간값/median-of-three-numbers.java
+++ 231119/세 수의 중간값/median-of-three-numbers.java
@@ -8,7 +8,7 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
         
-        if(b>a&&a<c) System.out.println(1);
+        if(b>a||a<c) System.out.println(1);
         else System.out.println(0);
 
     }
